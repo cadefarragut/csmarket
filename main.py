c42fd2cdc168eff4c26f633a991c14b17dd7af9e
cadefarragut <cade_farragut@yahoo.com>
2026-04-05 20:44:12 -0500

    ...

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ BASE_URL= "https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en
 SKINS_URL = f"{BASE_URL}/skins.json"
 WEARS_URL = f"{BASE_URL}/skins_not_grouped.json"
 
+my_inventory = []    
+
 def load_json(url, cache_file):
     if os.path.exists(cache_file):
         with open(cache_file, "r") as f:
@@ -29,26 +31,37 @@ def getInformation(skin_id):
                 "market_hash_name": skin["market_hash_name"],
             }
     return None
+
+def getInventory(steamid):
+    inventory = load_json(f"https://steamcommunity.com/inventory/{steamid}/730/2", "inv.json")
+    print(inventory)
+    for items in inventory["descriptions"]:
+        print(items["market_hash_name"])
+    #todo: Finish grabbing all information from inventory
+    
         
 
 
 def main():
-    m9_bayonet_stained_ft = getInformation("skin-bab9cbb31702_2")
-    ak47_sv_pant_ft = getInformation("skin-2faa35fd1c44_2_so")
-    m4a1_play_ft = getInformation("skin-4dd5dccd2fdd_2")
+    skins = load_json(SKINS_URL, "skins_cache.json")
+    wears = load_json(WEARS_URL, "wears_cache.json")
+
+    #m9_bayonet_stained_ft = getInformation("skin-bab9cbb31702_2")
+    #ak47_sv_pant_ft = getInformation("skin-2faa35fd1c44_2_so")
+    #m4a1_play_ft = getInformation("skin-4dd5dccd2fdd_2")
 
 
-    my_inventory.append(m9_bayonet_stained_ft)
-    my_inventory.append(ak47_sv_pant_ft)
-    my_inventory.append(m4a1_play_ft)
+    #my_inventory.append(m9_bayonet_stained_ft)
+    #my_inventory.append(ak47_sv_pant_ft)
+    #my_inventory.append(m4a1_play_ft)
+    getInventory("76561198146131977")
 
     inv = db.createDB("CadeInv.db")
     #for skins in my_inventory:
     inv.insertInv(my_inventory)
 
 
-my_inventory = []    
-skins = load_json(SKINS_URL, "skins_cache.json")
-wears = load_json(WEARS_URL, "wears_cache.json")
-main()
+
+if __name__ == "__main__":
+    main()
 
